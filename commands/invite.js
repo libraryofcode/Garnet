8943ef70c12c539f46040ed52bf5b9caa9d61d57
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-10 18:34:34 -0400

    Invite Command
    Add an invite command to the bot, currently only locked to testers.

---
+++ commands/invite.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+exports.run = async (client, message, args, level) => {
+    const embed = new Discord.RichEmbed()
+    .setAuthor(`${client.user.username}`, `${client.user.avatarURL}`)
+    .setColor(message.member.displayColor)
+    .addField("Invite Link", "https://discordapp.com/api/oauth2/authorize?client_id=460639060851949569&permissions=8&scope=bot", true)
+    .setFooter("Alpha/Development | ShadowShard")
+
+
+
+    message.channel.send(embed)
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ["get", "join"],
+    permLevel: "Systems Alpha/Dev Tester"
+  };
+  
+  exports.help = {
+    name: "invite",
+    category: "Miscelaneous",
+    description: "Provides the invite link.",
+    usage: "invite"
+  };
