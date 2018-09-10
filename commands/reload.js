6995e50ce0150e3b6be691cee404b6709d9c2652
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-10 19:16:35 -0400

    Fix the description fuckery, once again

--- commands/reload.js
+++ commands/reload.js
@@ -1,19 +1,19 @@
 exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
-  if (!args || args.length < 1) return message.reply("Please provide a package to reload.");
+  if (!args || args.length < 1) return message.reply("please provide a package to reload.");
 
   let response = await client.unloadCommand(args[0]);
-  if (response) return message.reply(`Error Unloading: ${response}`);
+  if (response) return message.reply(`Error Unloading: ${response}.`);
 
   response = client.loadCommand(args[0]);
-  if (response) return message.reply(`Error Loading: ${response}`);
+  if (response) return message.reply(`Error Loading: ${response}.`);
 
-  message.reply(`The package \`${args[0]}\` has been reloaded`);
+  message.reply(`the package \`${args[0]}\` has been reloaded.`);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ["r"],
   permLevel: "Systems Administrator"
 };
 
