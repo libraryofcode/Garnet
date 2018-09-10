9691b36447cb59da353c08c76957249f7c6888dd
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-10 19:11:58 -0400

    Fix eval desc fuckery

--- commands/eval.js
+++ commands/eval.js
@@ -20,7 +20,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["e"],
+  aliases: ["e", "exec"],
   permLevel: "Systems Administrator"
 };
 
