9ed2b5263d54c9c434844677f3a187cae2ddb7b4
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-10 19:10:08 -0400

    Add Alias to Eval

--- commands/eval.js
+++ commands/eval.js
@@ -20,7 +20,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ["e"],
   permLevel: "Systems Administrator"
 };
 
