2e244d4cfc83a27e97568e0253ee840cc9b64388
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-11 15:24:10 -0400

    Add Shiba Command (2)
    Thanks to TheSkele27.

---
+++ commands/shiba.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const superagent = require ("superagent");
+
+exports.run = async (client, message, args) => {
+    
+    let {body} = await superagent
+    .get ('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true');
+
+    let shibaembed = new Discord.RichEmbed()
+    .setColor("#f48c42")
+    .setTitle(":dog: Woof!")
+    .setImage(body[0]);
+
+    message.channel.send(shibaembed);
+}   
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["dog"],
+    permLevel: "Systems Alpha/Dev Tester"
+  };
+  
+  exports.help = {
+    name: "shiba",
+    category: "Fun",
+    description: "Provides a random picture of a dog.",
+    usage: "shiba"
+  };
