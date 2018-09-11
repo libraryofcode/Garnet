4f509e023b775e55afb65fc04e06a21494c43f61
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-11 17:28:24 -0400

    Addition of Administration Commands
    -Add a check invites command
    -Edit eval
    -Add exec

---
+++ commands/checkinvites.js
@@ -0,0 +1,18 @@
+exports.run = (client, msg, args, level) => {
+    const members = msg.guild.members.filter(member => member.user.presence.game && /(discord\.(gg|io|me|li)\/.+|discordapp\.com\/invite\/.+)/i.test(member.user.presence.game.name));
+    return msg.channel.send(members.map(member => `\`${member.id}\` ${member.displayName}`).join("\n") || "Nobody has an invite link as game name.");
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ["ci"],
+    permLevel: "Systems Alpha/Dev Tester"
+  };
+  
+  exports.help = {
+    name: 'checkinvites',
+    category: 'Moderation',
+    description: 'Returns a list of members with an invite as their game.',
+    usage: 'checkinvites'
+  };
