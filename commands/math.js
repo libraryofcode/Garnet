e4e48249cbbabf412dc344791f1a54cbaa07fe01
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-12 02:35:06 -0400

    Add a Math command
    Credit for this code goes to https://github.com/FlexLabs/Dyno-fun/blob/develop/src/commands/Math.js

---
+++ commands/math.js
@@ -0,0 +1,87 @@
+const superagent = require('superagent');
+const math = require('mathjs');
+
+async function request(i) {
+    return await superagent.get(`http://numbersapi.com/${i}?json`);
+}
+exports.run = async (client, message, args, level) => {
+    const input = args.join(' ');
+    const errmsg = {
+        exp: 'Couldn\'t evaluate the given expression',
+        err: 'An Error Occured',
+        fact: 'Couldn\'t get any facts for that number',
+    };
+    let res; // Used to get number facts
+    let output; // Output from mathjs
+    let result = {}; // Sends the result message
+        result.embed = {};
+        result.embed.fields = [];
+        result.embed.color = 0x41dae2;
+
+    if (args[0].toLowerCase() === 'fact') {
+        try {
+            res = await request('random/math');
+
+            if (res.body.found) {
+                result.embed.fields.push({
+                    name: `Fact about ${res.body.number}`,
+                    value: res.body.text,
+                    inline: false,
+                });
+            }
+        } catch (e) {
+            result = errmsg.err;
+        }
+
+        return message.channel.send(result);
+    }
+
+    if (args.length === 1 && !isNaN(args[0])) {
+            const ainput = (input.startsWith('.')) ? '0' + input : input;
+            res = await request(`${ainput.split('.')[0]}/math`);
+
+            if (res.body.found) {
+                result.embed.fields.push({
+                    name: `Fact about ${res.body.number}`,
+                    value: res.body.text,
+                    inline: false,
+                });
+            } else {
+                result = errmsg.fact;
+            }
+    } else {
+        try {
+            output = math.eval(input).toString();
+
+            result.embed.fields.push({
+                name: 'Input',
+                value: input,
+                inline: false,
+            });
+
+            result.embed.fields.push({
+                name: 'Output',
+                value: output,
+                inline: false,
+            });
+        } catch (e) {
+            result = errmsg.exp;
+        }
+    }
+
+    return message.channel.send(result);
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["calc"],
+    permLevel: "Systems Alpha/Dev Tester"
+  };
+  
+  exports.help = {
+    name: "math",
+    category: "Fun",
+    description: "Calculates a given mathematical expression.",
+    usage: "math [...args]"
+  };
