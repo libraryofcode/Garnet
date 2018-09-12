0b97c5249a4cf11f40743ecc90ef1d600fe0e083
Matthew R <40540813+mattsilver1@users.noreply.github.com>
2018-09-12 17:03:01 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -16,7 +16,7 @@ exports.run = async (client, message, args) => {
 };
 
   exports.conf = {
-    enabled: true,
+    enabled: false,
     guildOnly: true,
     aliases: [],
     permLevel: "Server Moderator"
