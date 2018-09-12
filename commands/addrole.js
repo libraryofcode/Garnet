3102ed72eeef6ed7115268865b7e45051ccef1cf
Matthew R <40540813+mattsilver1@users.noreply.github.com>
2018-09-12 17:02:37 -0400

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -17,7 +17,7 @@ exports.run = async (client, message, args) => {
 };
 
 exports.conf = {
-    enabled: true,
+    enabled: false,
     guildOnly: true,
     aliases: [],
     permLevel: "Server Admin"
