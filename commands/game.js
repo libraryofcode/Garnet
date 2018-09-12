522d230b9c630143b63915fe28bd0847165d9e4e
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-11 22:36:35 -0400

    game

--- commands/game.js
+++ commands/game.js
@@ -21,8 +21,23 @@ if (botuser.user.bot === true) {
   bot = "No";
 }
 
+if(!botuser.user.presence.game.assets.assets) {
     const embed = new Discord.RichEmbed()
-    .setAuthor(botuser.displayName, botuser.user.avatarURL)
+    .setAuthor(botuser.displayName, `undefined`)
+    .setColor(botuser.displayColor)
+    .addField("Status", `${status[botuser.user.presence.status]}`, true)
+    .addField("Playing", `${botuser.user.presence.game ? `${botuser.user.presence.game.name}` : "not playing anything."}`, true)
+    .addField("Details", `${botuser.user.presence.game.details}`, true)
+    .addField("State", `${botuser.user.presence.game.state}`, true)
+    .addField("ID", botuser.id, false)
+    .addField("Bot", `${bot}`, false)
+    .addField("Guild", `${bot}`, false)
+    .setFooter("ShadowShard | Alpha Development")
+    msg.edit(embed)   
+}
+    
+    const embed = new Discord.RichEmbed()
+    .setAuthor(botuser.displayName, botuser.user.presence.game.assets.smallImageURL)
     .setThumbnail(botuser.user.presence.game.assets.largeImageURL)
     .setColor(botuser.displayColor)
     .addField("Status", `${status[botuser.user.presence.status]}`, true)
