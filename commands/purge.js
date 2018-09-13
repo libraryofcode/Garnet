16dd4dd75e7aa63c0a33eefb4891877b693fa82a
TheSkele27 <33150773+TheSkele27@users.noreply.github.com>
2018-09-13 23:17:00 +0300

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,13 +1,13 @@
 exports.run = async (client, message, args) => {
 
   if (!message.member.hasPermission("MANAGE_MESSAGES")) 
-  return message.reply("Sorry, you don't have permissions to use this!");
+   return message.reply("Sorry, you don't have permissions to use this!");
     
   message.delete();
   const deleteCount = parseInt(args[0], 10);
     
   if (!deleteCount || deleteCount < 1 || deleteCount > 10000)
-    return message.reply("Please provide a number between 1 and 10000 for the number of messages to delete");
+  return message.reply("Please provide a number between 1 and 10000 for the number of messages to delete");
     
   const fetched = await message.channel.fetchMessages({limit: deleteCount});
   message.channel.bulkDelete(fetched)
