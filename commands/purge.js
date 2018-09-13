08eb3655757187c2eb5a1046c23b6727a54d76dd
TheSkele27 <33150773+TheSkele27@users.noreply.github.com>
2018-09-13 23:07:57 +0300

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,29 +1,29 @@
 exports.run = async (client, message, args) => {
 
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) 
-    return message.reply("Sorry, you don't have permissions to use this!");
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) 
+  return message.reply("Sorry, you don't have permissions to use this!");
     
-    message.delete();
-    const deleteCount = parseInt(args[0], 10);
+  message.delete();
+  const deleteCount = parseInt(args[0], 10);
     
-    if (!deleteCount || deleteCount < 1 || deleteCount > 10000)
-      return message.reply("Please provide a number between 1 and 10000 for the number of messages to delete");
+  if (!deleteCount || deleteCount < 1 || deleteCount > 10000)
+    return message.reply("Please provide a number between 1 and 10000 for the number of messages to delete");
     
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  const fetched = await message.channel.fetchMessages({limit: deleteCount});
+  message.channel.bulkDelete(fetched)
+    .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
 };
 
 exports.conf = {
-    enabled: true,
-    guildOnly: true,
-    aliases: [],
-    permLevel: "Server Moderator"
-  };
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: "Server Moderator"
+ };
   
-  exports.help = {
-    name: "purge",
-    category: "Moderation",
-    description: "Deletes the specified amount of messages.",
-    usage: "purge [number of messages]"
-  };
+exports.help = {
+  name: "purge",
+  category: "Moderation",
+  description: "Deletes the specified amount of messages.",
+  usage: "purge [number of messages]"
+};
