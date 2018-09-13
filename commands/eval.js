2b65b7dd5408abb083baee853e69da1d1a94f15e
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-13 17:19:10 -0400

    Fix eval fuckery

--- commands/eval.js
+++ commands/eval.js
@@ -1,11 +1,3 @@
-// The EVAL command will execute **ANY** arbitrary javascript code given to it.
-// THIS IS PERMISSION LEVEL 10 FOR A REASON! It's perm level 10 because eval
-// can be used to do **anything** on your machine, from stealing information to
-// purging the hard drive. DO NOT LET ANYONE ELSE USE THIS
-
-
-// However it's, like, super ultra useful for troubleshooting and doing stuff
-// you don't want to put in a command.
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const code = args.join(" ");
   try {
