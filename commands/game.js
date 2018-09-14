8cafdff9ea99aba0729a9a72704630bd5cfdb7b1
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-13 22:04:06 -0400

    Fix Bracket Fuckery

--- commands/game.js
+++ commands/game.js
@@ -14,40 +14,40 @@ exports.run = async (client, message, args) => {
     .setColor(botuser.displayColor)
     .setFooter(`${client.user.username} | ID ${botuser.id} | Alpha Development`);
     try {
-      embed.setAuthor(botuser.displayName, botuser.user.presence.game.assets.smallImageURL)
+      embed.setAuthor(botuser.displayName, botuser.user.presence.game.assets.smallImageURL);
   }
   catch(err) {
-      embed.setAuthor(botuser.displayName, 'https://cdn.discordapp.com/avatars/460639060851949569/4f545d7d0ee4fb31a411035793c4aef8.png?size=2048')
+      embed.setAuthor(botuser.displayName, 'https://cdn.discordapp.com/avatars/460639060851949569/4f545d7d0ee4fb31a411035793c4aef8.png?size=2048');
 }
     try {
-      embed.setThumbnail(botuser.user.presence.game.assets.largeImageURL)
+      embed.setThumbnail(botuser.user.presence.game.assets.largeImageURL);
     }
   catch(err) {
-    embed.setThumbnail('https://cdn.discordapp.com/avatars/460639060851949569/4f545d7d0ee4fb31a411035793c4aef8.png?size=2048')
+    embed.setThumbnail('https://cdn.discordapp.com/avatars/460639060851949569/4f545d7d0ee4fb31a411035793c4aef8.png?size=2048');
   }
   try {
-    embed.addField("Status", `${status[botuser.user.presence.status]}`, true)
+    embed.addField("Status", `${status[botuser.user.presence.status]}`, true);
   }
     catch(err) {
-      embed.addField("Status", `None`, true)
+      embed.addField("Status", `None`, true);
     }
   try {
-    embed.addField("Playing", `${botuser.user.presence.game ? `${botuser.user.presence.game.name}` : "Not playing anything"}`, true)
+    embed.addField("Playing", `${botuser.user.presence.game ? `${botuser.user.presence.game.name}` : "Not playing anything"}`, true);
   }
     catch(err) {
-      embed.addField("Playing", `This user is not playing anything.`, true)
+      embed.addField("Playing", `This user is not playing anything.`, true);
     }
   try {
-    embed.addField("Details", `${botuser.user.presence.game.details}`, true)
+    embed.addField("Details", `${botuser.user.presence.game.details}`, true);
   }
     catch(err) {
-      embed.addField("Details", `No details`, true)
+      embed.addField("Details", `No details`, true);
     }
   try {
-    embed.addField("State", `${botuser.user.presence.game.state}`, true)
+    embed.addField("State", `${botuser.user.presence.game.state}`, true);
   }
     catch(err) {
-      embed.addField("State", `No state`, true)
+      embed.addField("State", `No state`, true);
     }
   
   
