2005c9856e0deb4886b278b8a2a747ecf15813c9
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-13 22:01:45 -0400

    Fix bracket fuckery

--- commands/game.js
+++ commands/game.js
@@ -13,40 +13,40 @@ exports.run = async (client, message, args) => {
   const embed = new Discord.RichEmbed()
     .setColor(botuser.displayColor)
     .setFooter(`${client.user.username} | ID ${botuser.id} | Alpha Development`);
-    try{
+    try {
       embed.setAuthor(botuser.displayName, botuser.user.presence.game.assets.smallImageURL)
   }
-  catch(err){
+  catch(err) {
       embed.setAuthor(botuser.displayName, 'https://cdn.discordapp.com/avatars/460639060851949569/4f545d7d0ee4fb31a411035793c4aef8.png?size=2048')
 }
-    try{
+    try {
       embed.setThumbnail(botuser.user.presence.game.assets.largeImageURL)
     }
-  catch(err){
+  catch(err) {
     embed.setThumbnail('https://cdn.discordapp.com/avatars/460639060851949569/4f545d7d0ee4fb31a411035793c4aef8.png?size=2048')
   }
-  try{
+  try {
     embed.addField("Status", `${status[botuser.user.presence.status]}`, true)
   }
-    catch(err){
+    catch(err) {
       embed.addField("Status", `None`, true)
     }
-  try{
-    embed.addField("Playing", `${botuser.user.presence.game ? `${botuser.user.presence.game.name}` : "not playing anything."}`, true)
+  try {
+    embed.addField("Playing", `${botuser.user.presence.game ? `${botuser.user.presence.game.name}` : "Not playing anything"}`, true)
   }
-    catch(err){
+    catch(err) {
       embed.addField("Playing", `This user is not playing anything.`, true)
     }
-  try{
+  try {
     embed.addField("Details", `${botuser.user.presence.game.details}`, true)
   }
-    catch(err){
+    catch(err) {
       embed.addField("Details", `No details`, true)
     }
-  try{
+  try {
     embed.addField("State", `${botuser.user.presence.game.state}`, true)
   }
-    catch(err){
+    catch(err) {
       embed.addField("State", `No state`, true)
     }
   
