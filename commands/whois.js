518c2618380615070d468965d976d8e3b9bc3770
Kieran <31113295+CoalSephos@users.noreply.github.com>
2018-09-15 17:32:01 +0100

    fix formatting alerts / indent

--- commands/whois.js
+++ commands/whois.js
@@ -1,35 +1,34 @@
 const Discord = require("discord.js");
 const status = {
-    online: "Online",
-    idle: "Idle",
-    dnd: "Do Not Disturb",
-    offline: "Offline/Invisible"
-  };
+  online: "Online",
+  idle: "Idle",
+  dnd: "Do Not Disturb",
+  offline: "Offline/Invisible"
+};
 exports.run = async (client, message, args, level) => {
-    if (message.mentions.users.first())
-        try { 
-            level = client.permlevel(message.mentions.users.first().lastMessage);
-        } catch (e) {
-            level = 0;
-        }
-    try { const msg = await message.channel.send("Loading...");
+  if (message.mentions.users.first())
+    try {
+      level = client.permlevel(message.mentions.users.first().lastMessage);
+    } catch (e) {
+      level = 0;
+    }
+  try {
+    const msg = await message.channel.send("Loading...");
     const friendly = client.config.permLevels.find(l => l.level === level).name;
     const botuser = message.mentions.users.first() ? message.guild.members.get(message.mentions.users.first().id) : message.member;
     const matt = message.mentions.users.first() ? message.guild.members.get(message.mentions.users.first().id).roles.sort((a, b) => b.position - a.position).map(i => i.id).slice(0, -1) : message.member.roles.sort((a, b) => b.position - a.position).map(i => i.id).slice(0, -1);
-let myDick = "";
-for (let i = 0; i < matt.length; i++) {
+    let bot = "";
+    let myDick = "";
+    for (let i = 0; i < matt.length; i++) {
       myDick += "<@&" + matt[i] + ">";
-  if (matt.length != (i+1))
-    myDick += ", ";
-    var bot;
+      if (matt.length != (i + 1)) myDick += ", ";
+    }
     if (botuser.user.bot === true) {
       bot = "Yes";
     } else {
       bot = "No";
     }
-}
-myDick;
-   //lgtm [js/useless-expression]
+
     const embed = new Discord.RichEmbed()
       .setAuthor(botuser.displayName, botuser.user.avatarURL)
       .setColor(botuser.displayColor)
@@ -41,25 +40,25 @@ myDick;
       .addField("Acknowledgements", `${friendly}`, true)
       .addField("System Level", `${level}`, true)
       .setFooter(`${client.user.username} | ID ${botuser.id} |  Beta - Master`);
-      if (bot == "Yes") {
-        embed.addField("Bot", `${bot}`, true)
-      }
-    msg.edit(embed);
-} catch (err) {
-    msg.edit(`EXCPT*- ` 
-    + err);
+    if (bot == "Yes") {
+      embed.addField("Bot", `${bot}`, true)
     }
-  };
-  exports.conf = {
-    enabled: true,
-    guildOnly: true,
-    aliases: ["userinfo"],
-    permLevel: "Standard User"
-  };
-  
-  exports.help = {
-    name: "whois",
-    category: "Misc",
-    description: "Provides user information.",
-    usage: "whois"
-  };
+    msg.edit(embed);
+  } catch (err) {
+    msg.edit(`EXCPT*- ` +
+      err);
+  }
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["userinfo"],
+  permLevel: "Standard User"
+};
+
+exports.help = {
+  name: "whois",
+  category: "Misc",
+  description: "Provides user information.",
+  usage: "whois"
+};
