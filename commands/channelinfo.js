b1d39f3a7777506071f234d9f77a828cf35da7b3
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-17 00:22:08 -0400

    Fix Channel Info

--- commands/channelinfo.js
+++ commands/channelinfo.js
@@ -10,8 +10,12 @@ exports.run = async (client, message) => {
     .addField('Name', `${message.channel.name}`, true)
     .addField('Category', `${message.channel.parent.name}`, true)
     .addField('Topic', `${message.channel.topic}`, true)
-     
-    
+    if (message.channel.topic != true) {
+        embed.addField('Topic', 'None', true)
+    }
+    else {
+        embed.addField('Topic', `${message.channel.topic}`, true)
+    }
     embed.addField('Position', `${message.channel.calculatedPosition}`, true)
     embed.addField('Overrides', `${message.channel.permissionOverwrites.size}`, true)
     if (message.channel.nsfw === true) {
