a855a3b1cd467f7022c8d52701d5857448bd8e63
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-19 15:24:57 -0400

    Update Activated Servers

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,7 +4,7 @@ module.exports = (client, guild) => {
   /* this code checks to make sure that the bot is in the authorized list of servers below in the "activatedServers"
   If it's not in this list, then the bot will leave the server and then will return a DEBUG message to the console. */
 
-  const activatedServers = ["446067825673633794", "472913337454297108"]
+  const activatedServers = ["446067825673633794", "472913337454297108", "477182486586327060", "276019666399395850"]
   if (!activatedServers.includes()) {
     client.logger.debug(`[UNAUTHORIZED SERVER] ${guild.name} (${guild.id}) tried to add the bot, however the server is not activated. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`)
     guild.leave()
