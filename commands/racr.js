c4ee0bbd22a9c94a73cd867bb953eeb88efef1ab
Matthew R <40540813+mattsilver1@users.noreply.github.com>
2018-09-19 00:49:27 -0400

    Update racr.js

--- commands/racr.js
+++ commands/racr.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 
 exports.run = async (client, message, args) => {
 
-    const hook = new Discord.WebhookClient('491823610797883402', 'iKKbBlgqa7ooSScTL4jyK-s0MUCwM874C59gwS6G5MA1-CKp_e4KqCUj4uxMq5nK49O4');
+    const hook = new Discord.WebhookClient('491823610797883402', 'iKKbBlgqa7ooSScTL4jyK-s0MUCwM874C59gwS6G5MA1-CKp_****');
     message.react('✅')
     message.delete()
     hook.send(`Activation Request for server ${args[0]} by <@!${args[1]}> has been activated by ${message.author.username}#${message.author.discriminator}.`)
