d4da707b466eab0ff17f7488594fa93ac3cf3338
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-19 00:39:49 -0400

    Edit Activation Systems

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,7 +3,8 @@
 module.exports = (client, guild) => {
   /* this code checks to make sure that the bot is in the authorized list of servers below in the "activatedServers"
   If it's not in this list, then the bot will leave the server and then will return a DEBUG message to the console. */
-  const activatedServers = ["446067825673633794"]
+
+  const activatedServers = ["446067825673633794", "472913337454297108"]
   if (!activatedServers.includes()) {
     client.logger.debug(`[UNAUTHORIZED SERVER] ${guild.name} (${guild.id}) tried to add the bot, however the server is not activated. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`)
     guild.leave()
@@ -11,6 +12,7 @@ module.exports = (client, guild) => {
   else {
     return;
 }
+// If you don't want this feature on your bot, feel free to remove everything from this line of text to the above comment.
     
 
 
