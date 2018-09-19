dc8f1ae79ae8dc4ef7305ac85ef4851ba8d70712
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-19 00:43:01 -0400

    Push RAC data

---
+++ commands/rac.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js')
+
+
+exports.run = async (client, message, args) => {
+
+//https://canary.discordapp.com/api/webhooks/491814470641909765/p-OeOEnFzBGY9nnrLx-149wEHQbpmdGlMVKRQmbUJytcDtCT_Kw_AO24DY5KAcNimmes
+const hook = new Discord.WebhookClient('491814470641909765', 'p-OeOEnFzBGY9nnrLx-149wEHQbpmdGlMVKRQmbUJytcDtCT_Kw_AO24DY5KAcNimmes');
+
+// Send a message using the webhook
+hook.send(`__Activation request for server:__ 
+***${args[0]}*** initiated by user: **${message.author.username}#${message.author.discriminator}** with the ID of ${message.author.id}`);
+
+message.react('✅')
+message.channel.send(`***Server activation request for server ${args[0]} by ${message.author.username}#${message.author.discriminator} sent.***`)
+}
+
+
+
+
+
+
+
+
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ["request", "requestactivation"],
+    permLevel: "Standard User"
+  };
+  
+  exports.help = {
+    name: "rac",
+    category: "System",
+    description: "Sends a request to activate the server ID",
+    usage: "rac [...server ID]"
+  };
