56f10b3cfefea8d1cf39024be6f107f433918fd3
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-18 21:29:15 -0400

    Add Server Activation System

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,21 @@
 // This event executes when a new guild (server) is joined.
 
 module.exports = (client, guild) => {
+  /* this code checks to make sure that the bot is in the authorized list of servers below in the "activatedServers"
+  If it's not in this list, then the bot will leave the server and then will return a DEBUG message to the console. */
+  const activatedServers = ["446067825673633794"]
+  if (!activatedServers.includes()) {
+    client.logger.debug(`[UNAUTHORIZED SERVER] ${guild.name} (${guild.id}) tried to add the bot, however the server is not activated. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`)
+    guild.leave()
+  }
+  else {
+    return;
+}
+    
+
+
   client.logger.cmd(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`);
+
+
+
 };
