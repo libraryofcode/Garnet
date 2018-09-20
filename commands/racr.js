3afdd1de2f393824e49f4e54922832ec448d2ad1
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-19 20:54:49 -0400

    Update Guild Activation Service

--- commands/racr.js
+++ commands/racr.js
@@ -1,34 +1,54 @@
-const Discord = require('discord.js')
-
-
+const Discord = require("discord.js");
+const fs = require("fs");
 exports.run = async (client, message, args) => {
 
-    const hook = new Discord.WebhookClient('491823610797883402', 'iKKbBlgqa7ooSScTL4jyK-s0MUCwM874C59gwS6G5MA1-CKp_****');
-    message.react('✅')
-    message.delete()
-    hook.send(`Activation Request for server ${args[0]} by <@!${args[1]}> has been activated by ${message.author.username}#${message.author.discriminator}.`)
-    hook.send('If you need the invite URL, do `moon invite` in any channel and it will be sent to you.')
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    fs.readFile("./allowedGuildDB.json", "utf8", async (err, data) => { // readFile method basically allows us to read the data in that file
+        if (err !== null) { // Just an error checker
+            return console.log(err);
+        }
+        else {
+            const guildID = args[0];
+            if (data.length === 0) { // So let's use this case when the file data is empty
+                // So let's get the guild ID first. In eris, the arguments (Whatever follows the command name) is split by spaces, so it should be pretty easy to get the guild ID we need
+                const requiredObject = {
+                    allowedGuildIDs: []
+                }
+                requiredObject.allowedGuildIDs.push(guildID);
+                // After we push the data, we convert it to JSON
+                const json = JSON.stringify(requiredObject);
+                // And now we write the data to the same file
+                fs.writeFile("./allowedGuildDB.json", json, "utf8", (err) => {
+                    if (err !== null) {
+                        console.log(err);
+                    }
+                });
+                // Make sure you send message when this condition occurs too
+            }
+            else { // this case would mean that the file isn't empty
+                const requiredData = JSON.parse(data);
+                requiredData.allowedGuildIDs.push(guildID);
+                // And now we write the final data again
+                const json = JSON.stringify(requiredData);
+                fs.writeFile("./allowedGuildDB.json", json, "utf8", (err) => {
+                    if (err !== null) {
+                        console.log(err);
+                    }
+                    
+                });
+                message.delete()
+                message.channel.send(`✅ Moonglow has been activated on ${guildID} for <@!${args[1]}>`)
+                // I'm going to convert this over to discord.js really quick btw
+                // Alright
+            }
+        }
+    });    
+};
 
 exports.conf = {
     enabled: true,
     guildOnly: true,
     aliases: ["confirm"],
-    permLevel: "Systems Administrator"
+    permLevel: "Systems Support"
   };
   
   exports.help = {
--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,17 +1,24 @@
 // This event executes when a new guild (server) is joined.
-
+const fs = require("fs");
 module.exports = (client, guild) => {
   /* this code checks to make sure that the bot is in the authorized list of servers below in the "activatedServers"
   If it's not in this list, then the bot will leave the server and then will return a DEBUG message to the console. */
 
-  const activatedServers = ["446067825673633794", "472913337454297108", "477182486586327060", "276019666399395850", "475574326926770186"]
-  if (!activatedServers.includes()) {
-    client.logger.debug(`[UNAUTHORIZED SERVER] ${guild.name} (${guild.id}) tried to add the bot, however the server is not activated. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`)
-    guild.leave()
-  }
-  else {
-    return;
-}
+  fs.readFile("./allowedGuildDB.json", "utf8", (err, data) => {
+    if (err !== null) {
+        console.log(err);
+    }
+    else {
+        const allowedGuild = JSON.parse(data);
+        if (allowedGuild.allowedGuildIDs.includes(guild.id)) {
+            return;
+        }
+        else {
+            return guild.leave(guild.id);
+        }                                                            
+    }
+});
+
 // If you don't want this feature on your bot, feel free to remove everything from this line of text to the above comment.
     
 
