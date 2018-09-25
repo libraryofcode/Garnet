b22de2f3191e19570840fda5b42a89925d1c9828
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-25 05:22:13 -0400

    zz

--- commands/joe.js
+++ commands/joe.js
@@ -1,8 +1,12 @@
-exports.run = (client, message) => {
-    if(!message.member.userID === '208688963936845824' || '278620217221971968') {
+exports.run = (client, message, args) => {
+    if(message.author.id === '208688963936845824' || '278620217221971968') {
+        message.delete()
+        message.channel.send(`Joe have declared war on ${args[0]}, their anus will now be destroyed by the one and only... Virgin Slayer 69`);
+    }
+    else {
         return;
     }
-    message.channel.send(`Joe have declared war on ${args[0]}, their anus will now be destroyed by the one and only... Virgin Slayer 69`)
+
 }
 
 exports.conf = {
