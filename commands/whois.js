a145b349d7ae500e441870794d267f61b8f64584
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-25 23:38:49 -0400

    Allow IDs to be used in whois command
    Special thanks to Phoenix :)

--- commands/whois.js
+++ commands/whois.js
@@ -7,22 +7,23 @@ const status = {
   offline: "Offline/Invisible"
 };
 exports.run = async (client, message, args, level) => {
+  const resolvedUser = (args[0] !== undefined) ? message.guild.members.get(args[0].match(/[0-9]/g).join("")) : null;
   if (talkedRecently.has(message.author.id) && !message.member.roles.has("490364533550874644")) {
 
    const botmessage = await message.channel.send("You are being rate limited!" + message.author)
     botmessage.delete(10000)
   } else {
-  if (message.mentions.users.first())
+  if (resolvedUser)
     try {
-      level = client.permlevel(message.mentions.users.first().lastMessage);
+      level = client.permlevel(resolvedUser.lastMessage);
     } catch (e) {
       level = 0;
     }
   const msg = await message.channel.send("Loading...");
   try {
     const friendly = client.config.permLevels.find(l => l.level === level).name;
-    const botuser = message.mentions.users.first() ? message.guild.members.get(message.mentions.users.first().id) : message.member;
-    const matt = message.mentions.users.first() ? message.guild.members.get(message.mentions.users.first().id).roles.sort((a, b) => b.position - a.position).map(i => i.id).slice(0, -1) : message.member.roles.sort((a, b) => b.position - a.position).map(i => i.id).slice(0, -1);
+    const botuser = resolvedUser ? message.guild.members.get(resolvedUser.id) : message.member;
+    const matt = resolvedUser ? message.guild.members.get(resolvedUser.id).roles.sort((a, b) => b.position - a.position).map(i => i.id).slice(0, -1) : message.member.roles.sort((a, b) => b.position - a.position).map(i => i.id).slice(0, -1);
     let bot = "";
     let myDick = "";
     for (let i = 0; i < matt.length; i++) {
