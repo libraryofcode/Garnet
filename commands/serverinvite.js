86d633383daf8fcd919767799290ff33bd425885
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-26 22:17:01 -0400

    Update Server Invite

--- commands/serverinvite.js
+++ commands/serverinvite.js
@@ -5,6 +5,7 @@ exports.run = async (client, message, args, level) => {
     const embed = new Discord.RichEmbed()
     .setAuthor(message.guild.name, 'https://cdn.discordapp.com/icons/446067825673633794/19a38486fb2a04bda9bd064c6010ec06.png?size=128')
     .setTitle('Server Invitation')
+    .setDescription('Support server and main server for Moonglow')
     .setTimestamp(message.createdAt)
     .setFooter(client.user.username, client.user.avatarURL)
     .setDescription(invite)
