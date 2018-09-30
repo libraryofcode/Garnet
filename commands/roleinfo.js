4d05753e49c839335faf2aa792a15f8d4cf97327
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-30 01:13:36 -0400

    ESLINT roleinfo

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -1,22 +1,22 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 exports.run = async (client, message) => {
-    const mRole = message.mentions.roles.first()
-    const embed = new Discord.RichEmbed()
+  const mRole = message.mentions.roles.first();
+  const embed = new Discord.RichEmbed()
     .setAuthor(`${client.user.username}`, `${client.user.avatarURL}`)
-    .addField(`Test`, `${message.guild.roles.find(mRole).hexColor}`)
-    message.channel.send(embed)
-}
+    .addField('Test', `${message.guild.roles.find(mRole).hexColor}`);
+  message.channel.send(embed);
+};
 exports.conf = {
-    enabled: false,
-    guildOnly: true,
-    aliases: [],
-    permLevel: "Standard User"
-  };
+  enabled: false,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 'Standard User'
+};
   
-  exports.help = {
-    name: "roleinfo",
-    category: "Misc",
-    description: "Provides info about the mentioned role.",
-    usage: "roleinfo [...rolename]"
-  };
+exports.help = {
+  name: 'roleinfo',
+  category: 'Misc',
+  description: 'Provides info about the mentioned role.',
+  usage: 'roleinfo [...rolename]'
+};
   
