ec8086ec03b9d1cd60efbe1e5d0d1aa52d37723b
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-30 01:26:14 -0400

    ESLINT serverinvite

--- commands/serverinvite.js
+++ commands/serverinvite.js
@@ -1,27 +1,27 @@
-const Discord = require("discord.js");
-exports.run = async (client, message, args, level) => {
-    const invite = 'https://discord.gg/F7KXaFk'
+const Discord = require('discord.js');
+exports.run = async (client, message) => {
+  const invite = 'https://discord.gg/F7KXaFk';
 
-    const embed = new Discord.RichEmbed()
+  const embed = new Discord.RichEmbed()
     .setAuthor(message.guild.name, 'https://cdn.discordapp.com/icons/446067825673633794/19a38486fb2a04bda9bd064c6010ec06.png?size=128')
     .setTitle('Server Invitation')
     .setDescription('Support server and main server for Moonglow')
     .setTimestamp(message.createdAt)
     .setFooter(client.user.username, client.user.avatarURL)
-    .setDescription(invite)
-    message.channel.send(embed)
-}
+    .setDescription(invite);
+  message.channel.send(embed);
+};
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    permLevel: "Standard User"
-  };
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'Standard User'
+};
   
-  exports.help = {
-    name: "serverinvite",
-    category: "System",
-    description: "Posts the invite URL to the main server.",
-    usage: "serverinvite"
-  };
+exports.help = {
+  name: 'serverinvite',
+  category: 'System',
+  description: 'Posts the invite URL to the main server.',
+  usage: 'serverinvite'
+};
