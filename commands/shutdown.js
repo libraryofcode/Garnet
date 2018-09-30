550a343ab447f2eee8b2a50bb26c90809e78c2cd
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-30 01:32:29 -0400

    ESLINT shutdown

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,21 +1,21 @@
 exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
-  await message.reply("System is shutting down.");
+  await message.reply('System is shutting down.');
   client.commands.forEach( async cmd => {
     await client.unloadCommand(cmd);
   });
-  process.exit(1);
+  process.exit(80);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["s"],
-  permLevel: "Systems Administrator"
+  aliases: ['s'],
+  permLevel: 'Systems Administrator'
 };
 
 exports.help = {
-  name: "shutdown",
-  category: "System",
-  description: "Shuts down the bot.",
-  usage: "shutdown"
+  name: 'shutdown',
+  category: 'System',
+  description: 'Shuts down the bot.',
+  usage: 'shutdown'
 };
