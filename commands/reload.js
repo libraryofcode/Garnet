285c044e30b3ae04d0c944d1e2e9acf94f84904f
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-30 01:51:48 -0400

    Fix Reload Command

--- commands/reload.js
+++ commands/reload.js
@@ -1,5 +1,5 @@
-exports.run = (client, message, args, level) => {
-  if (!args || args.length < 1) return message.reply("please provide a package to reload.");
+exports.run = async (client, message, args) => {
+  if (!args || args.length < 1) return message.reply('please provide a package to reload.');
 
   let response = await client.unloadCommand(args[0]); //eslint-disable-line
   if (response) return message.reply(`Error Unloading: ${response}`);
@@ -8,18 +8,18 @@ exports.run = (client, message, args, level) => {
   if (response) return message.reply(`Error Loading: ${response}`);
 
   message.reply(`the package \`${args[0]}\` has been reloaded.`);
-}
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["r"],
-  permLevel: "Systems Administrator"
+  aliases: ['r'],
+  permLevel: 'Systems Administrator'
 };
 
 exports.help = {
-  name: "reload",
-  category: "System",
-  description: "Reloads a command that\"s been modified.",
-  usage: "reload [command]"
+  name: 'reload',
+  category: 'System',
+  description: 'Reloads a command that"s been modified.',
+  usage: 'reload [command]'
 };
