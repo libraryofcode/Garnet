c34052d507bcaf7174228947342e5002940266be
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-30 01:37:23 -0400

    ESLINT createinvite

--- commands/createinvite.js
+++ commands/createinvite.js
@@ -1,34 +1,32 @@
-const Discord = require('discord.js')
-
 exports.run = (client, message, args) => {
-    try {
-  if(args < 1) {message.channel.createInvite({
+  try {
+    if (args < 1) {message.channel.createInvite({
       maxAge: 0
-  })
-  .then(invite => message.channel.send(`Created an invite with a code of discord.gg/${invite.code}`))
-  .catch(console.error);
-}
-if(args > 1) {
-    message.channel.createInvite({
-        maxAge: args[0]
     })
-    .then(invite => message.channel.send(`Created invite with a code of discord.gg/${invite.code} with the age of ${args[0]} seconds`))
-}
-    } catch(err) {
-        message.channel.send(err)
+      .then(invite => message.channel.send(`Created an invite with a code of discord.gg/${invite.code}`))
+      .catch(console.error);
     }
-}
+    if (args > 1) {
+      message.channel.createInvite({
+        maxAge: args[0]
+      })
+        .then(invite => message.channel.send(`Created invite with a code of discord.gg/${invite.code} with the age of ${args[0]} seconds`));
+    }
+  } catch (err) {
+    message.channel.send(err);
+  }
+};
 
 exports.conf = {
-    enabled: true,
-    guildOnly: true,
-    aliases: [],
-    permLevel: "Standard User"
-  };
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 'Standard User'
+};
   
-  exports.help = {
-    name: 'createinvite',
-    category: 'Misc',
-    description: 'Creates an invite for the channel.',
-    usage: 'createinvite [...# of seconds]'
-  };
+exports.help = {
+  name: 'createinvite',
+  category: 'Misc',
+  description: 'Creates an invite for the channel.',
+  usage: 'createinvite [...# of seconds]'
+};
