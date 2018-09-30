34b912fb7547255a7d83fb577796d6899e34acb4
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-30 01:12:45 -0400

    Reload w/ Eslint

--- commands/reload.js
+++ commands/reload.js
@@ -1,14 +1,14 @@
-exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
+exports.run = (client, message, args, level) => {
   if (!args || args.length < 1) return message.reply("please provide a package to reload.");
 
-  let response = await client.unloadCommand(args[0]);
+  let response = await client.unloadCommand(args[0]); //eslint-disable-line
   if (response) return message.reply(`Error Unloading: ${response}`);
 
   response = client.loadCommand(args[0]);
   if (response) return message.reply(`Error Loading: ${response}`);
 
   message.reply(`the package \`${args[0]}\` has been reloaded.`);
-};
+}
 
 exports.conf = {
   enabled: true,
