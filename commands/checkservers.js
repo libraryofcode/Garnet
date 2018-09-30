4d72737ed76690b49196c7c6d487803e9ce3c678
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-09-30 01:36:50 -0400

    ESLINT checkservers

--- commands/checkservers.js
+++ commands/checkservers.js
@@ -1,18 +1,18 @@
-exports.run = (client, message, guild, msg) => {
-    const members = message.guild.members.filter(member => member.user.username && /(discord\.(gg|io|me|li)\/.+|discordapp\.com\/invite\/.+)/i.test(member.user.username));
-  return message.channel.send(members.map(member => `\`${member.id}\` ${member.displayName}`).join("\n") || "No one in this server has a Discord invite as their user name.");
-}
+exports.run = (client, message) => {
+  const members = message.guild.members.filter(member => member.user.username && /(discord\.(gg|io|me|li)\/.+|discordapp\.com\/invite\/.+)/i.test(member.user.username));
+  return message.channel.send(members.map(member => `\`${member.id}\` ${member.displayName}`).join('\n') || 'No one in this server has a Discord invite as their user name.');
+};
 
 exports.conf = {
-    enabled: true,
-    guildOnly: true,
-    aliases: ["cs"],
-    permLevel: "Server Moderator"
-  };
+  enabled: true,
+  guildOnly: true,
+  aliases: ['cs'],
+  permLevel: 'Server Moderator'
+};
   
-  exports.help = {
-    name: 'checkservers',
-    category: 'Moderation',
-    description: 'Returns a list of members with an invite as their username.',
-    usage: 'checkservers'
-  };
+exports.help = {
+  name: 'checkservers',
+  category: 'Moderation',
+  description: 'Returns a list of members with an invite as their username.',
+  usage: 'checkservers'
+};
