b0f63cf9ee5291aa1edeb948686d690a7e35f549
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-01 02:24:22 -0400

    Fix roleinfo

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -1,10 +1,25 @@
 const Discord = require('discord.js');
-exports.run = async (client, message) => {
-  const mRole = message.mentions.roles.first();
+exports.run = async (client, message, args) => {
+  const msg = await message.channel.send('Loading...');
+  const roleM = message.guild.roles.find(role => role.name.toLowerCase() === args.join(' ').toLowerCase());
+  
   const embed = new Discord.RichEmbed()
-    .setAuthor(`${client.user.username}`, `${client.user.avatarURL}`)
-    .addField('Test', `${message.guild.roles.find(mRole).hexColor}`);
-  message.channel.send(embed);
+  embed.setAuthor(client.user.username, client.user.avatarURL);
+  embed.setTitle(`Role | ${roleM.name}`);
+  embed.setColor(`${roleM.hexColor}`);
+  embed.addField('Members', `${roleM.members.size}`, true);
+  embed.addField('Color', `${roleM.hexColor}`, true);
+  embed.addField('Position', `${roleM.position}`, true);
+  embed.addField('Hoisted', `${roleM.hoist}`, true);
+  embed.addField('Mentionable', `${roleM.mentionable}`, true);
+  embed.addField('Created At', `${roleM.createdAt.toLocaleString('en-US')}`, true);
+  embed.setFooter(`${client.user.username} | Role ID: ${roleM.id}`);
+  embed.setTimestamp();
+
+  msg.edit(embed)
+
+
+
 };
 exports.conf = {
   enabled: false,
