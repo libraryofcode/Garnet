80030ef1b855347228b8c0f580bff1c0021f67d4
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-01 17:24:59 -0400

    Finish members command

--- commands/members.js
+++ commands/members.js
@@ -4,11 +4,12 @@ exports.run = async (client, message, args) => {
   const msg = await message.channel.send('Loading...');
   const roleM = message.guild.roles.find(role => role.name.toLowerCase() === args.join(' ').toLowerCase());
 
-  const embed = new Discord.RichEmbed()
-    .setAuthor(client.user.username, client.user.avatarURL)
-    .setTitle(`Members | ${roleM.name}`)
-    .setDescription(`${roleM.members.map(m=>m.user.tag).join('\n')}`)
-    .setFooter(`${client.user.username} | Role ID: ${roleM.id}`);
+  const embed = new Discord.RichEmbed();
+  embed.setAuthor(client.user.username, client.user.avatarURL);
+  embed.setTitle(`Members | ${roleM.name}`);
+  embed.setDescription(`${roleM.members.map(m=>m.user.tag).join('\n')}`);
+  embed.setTimestamp();
+  embed.setFooter(`${client.user.username} | Role ID: ${roleM.id}`);
   
 
   msg.edit(embed);
