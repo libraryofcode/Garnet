1edc15b6cd794d28a04ef7e5bf1ea6bbf9b59df6
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-01 02:36:02 -0400

    Make roleinfo look nicer

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -2,21 +2,33 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
   const msg = await message.channel.send('Loading...');
   const roleM = message.guild.roles.find(role => role.name.toLowerCase() === args.join(' ').toLowerCase());
+
   
-  const embed = new Discord.RichEmbed()
+  const embed = new Discord.RichEmbed();
+
   embed.setAuthor(client.user.username, client.user.avatarURL);
-  embed.setTitle(`Role | ${roleM.name}`);
+  embed.setTitle(`Role | **${roleM.name}**`);
   embed.setColor(`${roleM.hexColor}`);
   embed.addField('Members', `${roleM.members.size}`, true);
-  embed.addField('Color', `${roleM.hexColor}`, true);
+  embed.addField('Color', `${roleM.hexColor.toUpperCase()}`, true);
   embed.addField('Position', `${roleM.position}`, true);
-  embed.addField('Hoisted', `${roleM.hoist}`, true);
-  embed.addField('Mentionable', `${roleM.mentionable}`, true);
+  if (roleM.hoist === false) {
+    embed.addField('Hoisted', 'False', true);
+  }
+  else {
+    embed.addField('Hoisted', 'True', true);
+  }
+  if (roleM.mentionable == false) {
+    embed.addField('Mentionable', 'False', true);
+  }
+  else {
+    embed.addField('Mentionable', 'True', true)
+  }
   embed.addField('Created At', `${roleM.createdAt.toLocaleString('en-US')}`, true);
   embed.setFooter(`${client.user.username} | Role ID: ${roleM.id}`);
   embed.setTimestamp();
 
-  msg.edit(embed)
+  msg.edit(embed);
 
 
 
