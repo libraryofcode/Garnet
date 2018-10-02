8f01cfefe7b707a5bb3e1f4d11102e798c088e93
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-02 00:10:24 -0400

    Give the Members CMD a color

--- commands/members.js
+++ commands/members.js
@@ -7,6 +7,9 @@ exports.run = async (client, message, args) => {
   const embed = new Discord.RichEmbed();
   embed.setAuthor(client.user.username, client.user.avatarURL);
   embed.setTitle(`Members | **${roleM.name}**`);
+  if (roleM.hexColor) {
+    embed.setColor(`${roleM.hexColor}`)
+  }
   embed.setDescription(`${roleM.members.map(m=>m.user.tag).join('\n')}`);
   embed.setTimestamp();
   embed.setFooter(`${client.user.username} | Role ID: ${roleM.id}`);
