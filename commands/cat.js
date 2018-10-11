86cd6b39906077a8ed24f1f0adde628461b48f37
TheSkele27 <33150773+TheSkele27@users.noreply.github.com>
2018-10-11 21:37:45 +0300

    Create cat.js

---
+++ commands/cat.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const superagent = require ("superagent");
+
+module.exports.run = async (client, message, arges) => {
+    
+    let {body} = await superagent
+    .get('http://aws.random.cat/meow');
+
+    let catembed = new Discord.RichEmbed()
+    .setColor("#f48c42")
+    .setTitle(":cat: Meow")
+    .setImage(body.file);
+
+    message.channel.send(catembed);
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'Standard User'
+};
+  
+exports.help = {
+  name: 'cat',
+  category: 'Fun',
+  description: 'Provides a random picture of a cat.',
+  usage: 'cat'
+};
