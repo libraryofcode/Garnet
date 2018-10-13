e611e20f8f7196ea79bcb80f75f0def687446b10
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-13 19:45:41 -0400

    Add a membercount command

---
+++ commands/membercount.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message) => {
+  function checkBots(guild) {
+    let botCount = 0; 
+    guild.members.forEach(member => { 
+      if (member.user.bot) botCount++; 
+    });
+    return botCount; 
+  }
+  function checkMembers(guild) {
+    let memberCount = 0;
+    guild.members.forEach(member => {
+      if (!member.user.bot) memberCount++; 
+    });
+    return memberCount;
+  }
+
+
+  const embed = new Discord.RichEmbed();
+  embed.setAuthor(client.user.username, client.user.avatarURL);
+  embed.setTitle('Server Membercount');
+  embed.addField('Total Members', message.guild.memberCount, true);
+  embed.addField('Humans', checkMembers(message.guild), true);
+  embed.addField('Bots', checkBots(message.guild), true);
+  message.guild.fetchBans()
+    .then(bans => embed.addField('Bans', `${bans.size}`));
+  embed.setTimestamp();
+  embed.setFooter(`${client.user.username} | Server ID: ${message.guild.id}`);
+  message.channel.send(embed);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 'Standard User'
+};
+  
+exports.help = {
+  name: 'membercount',
+  category: 'Moderation',
+  description: 'Shows a full membercount.',
+  usage: 'membercount'
+};
+
