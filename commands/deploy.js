dc8afb17ddae1bd3f9ca61f12b6e113d8f8fc473
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-14 14:53:09 -0400

    Create a Deploy Command

---
+++ commands/deploy.js
@@ -0,0 +1,43 @@
+const { exec } = require('child_process');
+
+exports.run = async (client, msg, args) => {
+  const command = 'git pull';
+  //args.join(' ');
+  const outMessage = await msg.channel.send('`Deploying...`');
+  //await msg.channel.send(`Running \`${command}\`...`);
+  let stdOut = await doExec(command).catch(data=> outputErr(outMessage, data));
+  stdOut = stdOut.substring(0, 1750);
+  outMessage.edit(`\`DEPLOYMENT STATUS\`
+\`\`\`sh
+${stdOut}
+\`\`\``);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'Systems Administrator'
+};
+
+exports.help = {
+  name: 'deploy',
+  category: 'System',
+  description: 'Deploys recent changes on the repository.',
+  usage: 'deploy'
+};
+
+const outputErr = (msg, stdData) => {
+  const { stdout, stderr } = stdData;
+  const message = stdout.concat(`\`\`\`${stderr}\`\`\``);
+  msg.edit(message);
+};
+
+const doExec = (cmd, opts = {}) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, opts, (err, stdout, stderr) => {
+      if (err) return reject({ stdout, stderr });
+      resolve(stdout);
+    });
+  });
+};
