2e47175fda87c908108d5979bed6a3ce859fd210
Liz <42483589+Krunniee@users.noreply.github.com>
2018-10-16 15:29:51 -0400

    Update error.js

--- events/error.js
+++ events/error.js
@@ -1,3 +1,4 @@
 module.exports = async (client, error) => {
-  client.logger.log(`An error event was sent by Discord.js: \n${JSON.stringify(error)}`, "error");
+  console.warn("An unknown error has occured, please read the following log message.")
+  await client.logger.log(`An error event was sent by Discord.js: \n${JSON.stringify(error)}`, "error");
 };
