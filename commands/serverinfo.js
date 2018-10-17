1b6daf99c48c393144bbf9aeff0949819878dd22
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-17 00:15:13 -0400

    Make `serverinfo` look nicer

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -5,7 +5,7 @@ exports.run = async (client, message) => {
 
     message.channel.send('You are being rate limited!' + message.author);
   } else { // eslint-disable-line no-unused-vars
-    const msg = await message.channel.send('Loading... *If you are seeing this message longer than 5 seconds, an error may have occurred.*');
+    const msg = await message.channel.send('Loading...');
     function checkBots(guild) {
       let botCount = 0; 
       guild.members.forEach(member => { 
@@ -20,15 +20,100 @@ exports.run = async (client, message) => {
       });
       return memberCount;
     }
+
+    const guild = message.guild;
+    function checkRegion(message, guild) {
+      const regionArray = [];
+      if (message.guild.region === 'us-east' ) {
+        regionArray.push('US East');
+      }
+      if (message.guild.region === 'vip-us-east') {
+        regionArray.push('US East `VIP`');
+      }
+      if (message.guild.region === 'us-south') {
+        regionArray.push('US South');
+      }
+      if (message.guild.region === 'vip-us-south') {
+        regionArray.push('US South `VIP`');
+      }
+      if (message.guild.region === 'us-west') {
+        regionArray.push('US West');
+      }
+      if (message.guild.region === 'vip-us-west') {
+        regionArray.push('US West `VIP`');
+      }
+      if (message.guild.region === 'us-central') {
+        regionArray.push('US Central');
+      }
+      if (message.guild.region === 'vip-us-central') {
+        regionArray.push('US Central `VIP`');
+      }
+      if (message.guild.region === 'sydney') {
+        regionArray.push('Sydney');
+      }
+      if (message.guild.region === 'vip-sydney') {
+        regionArray.push('Sydney `VIP`');
+      }
+      if (message.guild.region === 'southafrica') {
+        regionArray.push('South Africa');
+      }
+      if (message.guild.region === 'vip-southafrica') {
+        regionArray.push('South Africa `VIP`');
+      }
+      if (message.guild.region === 'singapore') {
+        regionArray.push('Singapore');
+      }
+      if (message.guild.region === 'vip-singapore') {
+        regionArray.push('Singapore `VIP`');
+      }
+      if (message.guild.region === 'russia') {
+        regionArray.push('Russia');
+      }
+      if (message.guild.region === 'vip-russia') {
+        regionArray.push('Russia `VIP`');
+      }
+      if (message.guild.region === 'japan') {
+        regionArray.push('Japan');
+      }
+      if (message.guild.region === 'vip-japan') {
+        regionArray.push('Japan `VIP`');
+      }
+      if (message.guild.region === 'hongkong') {
+        regionArray.push('Hong Kong');
+      }
+      if (message.guild.region === 'vip-hongkong') {
+        regionArray.push('Hong Kong `VIP`');
+      }
+      if (message.guild.region === 'eu-west') {
+        regionArray.push('EU West');
+      }
+      if (message.guild.region === 'vip-eu-west') {
+        regionArray.push('EU West `VIP`');
+      }
+      if (message.guild.region === 'eu-east') {
+        regionArray.push('EU East');
+      }
+      if (message.guild.region === 'vip-eu-east') {
+        regionArray.push('EU East `VIP`');
+      }
+      if (message.guild.region === 'brazil') {
+        regionArray.push('Brazil');
+      }
+      if (message.guild.region === 'vip-brazil') {
+        regionArray.push('Brazil `VIP`');
+      }
+
+      return regionArray;
+    }
     const ownerUsername = message.guild.members.get(message.guild.owner.id).user.username;
     const ownerDiscrim = message.guild.members.get(message.guild.owner.id).user.discriminator;
     const embed = new Discord.RichEmbed()
-      .setAuthor(`${client.user.username}`, `${client.user.avatarURL}`)
+      //.setAuthor(`${client.user.username}`, `${client.user.avatarURL}`)
       .setTitle('Server Information')
       .setThumbnail(message.guild.iconURL)
       //.setColor(message.member.displayColor)
       .addField('Server Owner', `${ownerUsername}#${ownerDiscrim}`, true)
-      .addField('Server Region', message.guild.region, true)
+      .addField('Server Region', `${checkRegion(message, guild)}`, true)
       .addField('Created', `${message.guild.createdAt.toLocaleString('en-US')}`, true)
       .addField('Roles', `${message.guild.roles.size}`, true)
       .addField('Members', message.guild.memberCount, true)
@@ -49,8 +134,9 @@ exports.run = async (client, message) => {
     if (message.guild.verificationLevel === 4) {
       embed.addField('Verification Level', 'High', true);
     }
+    embed.setTimestamp();
     //.addField('Verification Level', message.guild.verificationLevel, true)
-    embed.setFooter(`${client.user.username} | Beta - Master`);
+    embed.setFooter(`${client.user.username}`, client.user.avatarURL);
     msg.edit(embed);
     talkedRecently.add(message.author.id);
     setTimeout(() => {
