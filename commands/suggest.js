c73e15aaab24d5b75d44751f85825f9342c0a1c5
Matthew R <40540813+mattsilver1@users.noreply.github.com>
2018-10-19 20:10:08 -0400

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
-  const dataSuggestion = args.join(" ");
+  const dataSuggestion = args.join(' ');
   if (!dataSuggestion) return message.reply('**Please include text for the suggestion!**');
   
   const Discord = new Discord.RichEmbed()
-  .setTitle('FCC Server Suggestion')
+  .setTitle('FC Server Suggestion')
   .addField('Author:', `${message.author.username} (${message.author.id})`)
   .addField('Suggestion:', `${dataSuggestion}`)
   .setTimestamp()
-  .setColor('BLACK')
+  .setColor('RANDOM')
+  .setFooter(client.user.username, client.user.avatarURL)
   client.channels.get('462361967026241536').send({embed});
 };
 
@@ -21,7 +22,7 @@ exports.conf = {
   
 exports.help = {
   name: 'suggest',
-  category: 'FCC Server',
+  category: 'system',
   description: 'Suggest something on the guild.',
   usage: 'suggest [..suggestion]'
 };
