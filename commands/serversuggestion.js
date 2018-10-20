874888cf4804ba30386a28f42dc37e5f5df9b0e6
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-19 20:32:25 -0400

    Add as server suggestion command

--- commands/serversuggestion.js
+++ commands/serversuggestion.js
@@ -3,7 +3,8 @@ exports.run = async (client, message, args) => {
   const dataSuggestion = args.join(' ');
   if (!dataSuggestion) return message.reply('**Please include text for the suggestion!**');
 
-  const msg = await message.channel.send('Sending your suggestion to Development...');
+  const msg = await message.channel.send('Sending your suggestion');
+  if (!message.guild.id === '446067825673633794') return msg.edit('This command can only be used in the Fortnite Community server.');
   
   const embed = new Discord.RichEmbed()
     .setTitle('Moonglow Suggestion')
@@ -12,8 +13,8 @@ exports.run = async (client, message, args) => {
     .setTimestamp()
     .setColor('RANDOM')
     .setFooter(client.user.username, client.user.avatarURL);
-  client.channels.get('481464215962648577').send({embed});
-  msg.edit('Suggestion has been successfully sent to the Moonglow Development Team!');
+  client.channels.get('462361967026241536').send({embed});
+  msg.edit('Suggestion has been successfully sent!');
   msg.delete(10000);
   message.delete(10000);
   //pls
@@ -27,8 +28,8 @@ exports.conf = {
 };
   
 exports.help = {
-  name: 'suggest',
-  category: 'system',
-  description: 'Send a suggestion for Moonglow to the developers.',
+  name: 'serversuggest',
+  category: 'settings',
+  description: 'Send a suggestion for the FC Server.',
   usage: 'suggest [..suggestion]'
 };
