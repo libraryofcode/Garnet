9319f6fdb2ca456bc3e4de160019737fe8ab1d30
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-20 22:44:21 -0400

    Add 51 messageDelete logs

---
+++ events/messageDelete.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+module.exports = async (client, message, member, guild) => { //eslint-disable-line no-unused-vars
+  const memberName = message.member.user.tag;
+  const memberID = message.member.id;
+  const guildName = message.guild.name;
+  const guildID = message.guild.id;
+  const content = message.content;
+  const createdAt = message.createdAt;
+  const channelName = message.channel.name;
+  const channelID = message.channel.id;
+
+  const embed = new Discord.RichEmbed()
+    .setTitle('Message Deletion Event')
+    .setDescription(`**CONTENT**\n\n${content}`)
+    .addField('User', `${memberName} \`(${memberID})\``, true)
+    .addField('Guild', `${guildName} \`(${guildID})\``, true)
+    .addField('Channel', `${channelName} \`(${channelID})\``, true)
+    .addField('Created At', createdAt, true)
+    .setFooter(client.user.username, client.user.avatarURL)
+    .setTimestamp();
+  client.channels.get('503373884251308042').send(embed);
+
+
+};
