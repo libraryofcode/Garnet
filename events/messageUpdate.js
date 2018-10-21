0427015bfebea6d6d1a134336e3e0de7a6d22858
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-21 01:14:30 -0400

    pls

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -12,8 +12,8 @@ module.exports = async (client, message, oldMessage) => { //eslint-disable-line 
   if (oldContent === newContent) return;
   
   const embed = new Discord.RichEmbed()
-    .setTitle('Message Edited Event');
-  if (!message.guild.id === '203039963636301824') embed.setDescription(`**OLD CONTENT**\n\n\`\`\`${oldContent}\`\`\`\n\n**NEW CONTENT**\n\n\`\`\`${newContent}\`\`\``)
+    .setTitle('Message Edited Event')
+    .setDescription(`**OLD CONTENT**\n\n\`\`\`${oldContent}\`\`\`\n\n**NEW CONTENT**\n\n\`\`\`${newContent}\`\`\``)
     .addField('User', `${memberName} \`(${memberID})\``, true)
     .addField('Guild', `${guildName} \`(${guildID})\``, true)
     .addField('Channel', `${channelName} \`(${channelID})\``, true)
