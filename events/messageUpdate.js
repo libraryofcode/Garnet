d6afdf3fccad48e397b287e262b7d79ac6a198e4
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-20 22:44:30 -0400

    Add 51 messageUpdate logs

---
+++ events/messageUpdate.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+module.exports = async (client, message, oldMessage) => { //eslint-disable-line no-unused-vars
+  const memberName = message.member.user.tag;
+  const memberID = message.member.id;
+  const guildName = message.guild.name;
+  const guildID = message.guild.id;
+  const createdAt = message.createdAt;
+  const channelName = message.channel.name;
+  const channelID = message.channel.id;
+  const oldContent = oldMessage.content;
+  const newContent = message.content;
+  if (oldContent === newContent) return;
+  
+  const embed = new Discord.RichEmbed()
+    .setTitle('Message Deletion Event')
+    .setDescription(`**OLD CONTENT**\n\n\`\`\`${oldContent}\`\`\`\n\n**NEW CONTENT**\n\n\`\`\`${newContent}\`\`\``)
+    .addField('User', `${memberName} \`(${memberID})\``, true)
+    .addField('Guild', `${guildName} \`(${guildID})\``, true)
+    .addField('Channel', `${channelName} \`(${channelID})\``, true)
+    .addField('Created At', createdAt, true)
+    .setFooter(client.user.username, client.user.avatarURL)
+    .setTimestamp();
+  client.channels.get('503387318162292736').send(embed);
+}
