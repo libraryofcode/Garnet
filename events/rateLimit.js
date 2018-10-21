62f8be44b31c65232d1951c33d5a3f8632b804c3
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-10-21 02:56:21 -0400

    Fix rateLimit

--- events/rateLimit.js
+++ events/rateLimit.js
@@ -1,18 +1,19 @@
 const Discord = require('discord.js');
 module.exports = (client, guild, message, rateLimit, rateLimitInfo) => { //eslint-disable-line no-unused-vars
   const info = rateLimitInfo;
-  const requestLimit = rateLimitInfo.requestLimit;
+  /*const requestLimit = rateLimitInfo.requestLimit;
   const timeDifference = rateLimitInfo.timeDifference;
   const method = rateLimitInfo.method;
   const path = rateLimitInfo.path;
+  */
   
   const embed = new Discord.RichEmbed();
   embed.setTitle('Rate Limit Event');
   embed.setDescription(info);
-  embed.addField('Request Limit', requestLimit, true);
-  embed.addField('Time Difference', timeDifference, true);
-  embed.addField('Method', method, true);
-  embed.addField('Path', path, true);
+  //embed.addField('Request Limit', requestLimit, true);
+  //embed.addField('Time Difference', timeDifference, true);
+  //embed.addField('Method', method, true);
+  //embed.addField('Path', path, true);
   embed.setFooter(client.user.username, client.user.avatarURL);
   embed.setTimestamp();
 
