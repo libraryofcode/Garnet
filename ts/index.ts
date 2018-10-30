c779eb13a39d13617c82fb1abc7fc56025d5c0c1
Confoederatio <42483589+DarkCelvestian@users.noreply.github.com>
2018-10-29 20:05:52 -0400

    Update index.ts

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,11 @@
 //const Discord = require('discord.js');
-
+const Enmap = require('enmap');
+const Database = require('enmap-sqlite)(Enmap)
+                     
 const mainDeveloper : string = 'Matthew#0008'
 const developers = ['FlatBird, NightRaven, CoalSephos'];
 const version : string = '4.5.0';
 const typings : string = './typings'
+const db = new Database();
 
 module.exports = 'details';
