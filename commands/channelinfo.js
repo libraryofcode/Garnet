69adbb783faab3d3dc6aecbb7e57749a084a23e9
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-11-04 12:50:33 -0500

    Allow channelinfo to get other channel data

--- commands/channelinfo.js
+++ commands/channelinfo.js
@@ -1,13 +1,17 @@
 const Discord = require('discord.js');
 
-exports.run = async (client, message) => {
+exports.run = async (client, message, args) => {
+
+  const resolvedChannel = (args[0] !== undefined) ? message.guild.channels.get(args[0].match(/[0-9]/g).join('')) : null;
+  const thisChannel = resolvedChannel ? message.guild.channels.get(resolvedChannel.id) : message.channel;
+
   const msg = await message.channel.send('Loading...');
   const embed = new Discord.RichEmbed()
     .setAuthor(message.guild.name, message.guild.iconURL)
     .setTitle('Channel Information')
-    .addField('Name', `${message.channel.name}`, true);
+    .addField('Name', `${thisChannel.name}`, true);
   try { 
-    embed.addField('Category', `${message.channel.parent.name}`, true);
+    embed.addField('Category', `${thisChannel.parent.name}`, true);
   } catch (err) {
     embed.addField('Category', 'None', true);
   }
@@ -15,11 +19,11 @@ exports.run = async (client, message) => {
     embed.addField('Topic', 'None', true);
   }
   else {
-    embed.addField('Topic', `${message.channel.topic}`, true);
+    embed.addField('Topic', `${thisChannel.topic}`, true);
   }
-  embed.addField('Position', `${message.channel.calculatedPosition}`, true);
-  embed.addField('Overrides', `${message.channel.permissionOverwrites.size}`, true);
-  if (message.channel.nsfw === true) {
+  embed.addField('Position', `${thisChannel.calculatedPosition}`, true);
+  embed.addField('Overrides', `${thisChannel.permissionOverwrites.size}`, true);
+  if (thisChannel.nsfw === true) {
     embed.addField('NSFW', 'Yes', true);
   }
   embed.setFooter(client.user.username, client.user.avatarURL);
