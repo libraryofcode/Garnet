f27e4fc7363a3e06b98178f63bd44e1be8769e4b
Matthew R <40540813+mattsilver1@users.noreply.github.com>
2018-11-07 08:27:39 -0500

    Update utils.js

--- util/utils.js
+++ util/utils.js
@@ -5,16 +5,22 @@ class utils {
        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
       });
     }  
+    
+    //Makes the string in the function proper case. For example, "hi how are you today" would become "Hi How Are You Today".
 
     this.toInvertedCase = (string)  => {
       return string.replace(/([^\W_]+[^\s-]*) */g, (txt) => {
        return txt.charAt(0).toLowerCase() + txt.substr(1).toUpperCase()
       });
     } 
+    
+    //Does the opposite of toProperCase, makes it inverted. Like "hI hOW aRE yOU tODAY".
 
     this.random = function() {
       return this[Math.floor(Math.random() * this.length)];
     };
+    
+    //Gets a random element from an array.
     
     
     
