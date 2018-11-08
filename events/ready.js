face64d1ec0775111e2f35c0db00301e70204c98
Matthew R <40540813+mattsilver1@users.noreply.github.com>
2018-11-08 11:55:05 -0500

    Remove comments & compact

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,8 @@
 const Discord = require('discord.js');
 const web = require('../webhooks.json');
 module.exports = async client => {
-  // Log that the bot is online.
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, 'ready');
-
-  // Make the bot "play the game" which is the help command with default prefix.
   client.user.setActivity(`${client.config.defaultSettings.prefix}help | ${client.users.size} users`, {type: 'PLAYING'});
-  //client.channels.get('503384677227429888').send(`\`\`\`js\n\n${client.user.tag} is now online in ${client.guilds.size} servers with ${client.users.size} users.\`\`\``);
   const hook = new Discord.WebhookClient(web.mainLogID, web.mainLogToken);
   const embed = new Discord.RichEmbed()
     .setTitle('Ready Event')
@@ -19,7 +15,6 @@ module.exports = async client => {
     .setFooter(client.user.username, client.user.avatarURL)
     .setTimestamp();
     
-  //client.channels.get('503384677227429888').send(embed);
   hook.send(embed);
     
 
