9167e12a4441ccd5d3df38e728f4dfef610c0833
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-11-15 16:43:13 -0500

    fix

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,8 @@
 //const Discord = require('discord.js');
 
 const mainDeveloper : string = 'Matthew#0008'
-const developers = ['FlatBird, NightRaven, CoalSephos'];
-const version : string = '4.5.0';
+const developers : string[] = ['FlatBird, NightRaven, CoalSephos'];
+const version : number = 4.5;
 const typings : string = './typings'
 
 module.exports = 'details';
