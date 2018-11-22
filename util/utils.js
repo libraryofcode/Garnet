dc3f61c9583d91f279e6749c5fdce4c67e638741
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-11-21 21:24:31 -0500

    Version commands and utility

--- util/utils.js
+++ util/utils.js
@@ -1,19 +1,26 @@
+const thisPackage = require('../package.json');
+const thisPackageLock = require('../package-lock.json');
+
 class utils {
-  constructor(toProperCase, random, wait) {
+  constructor(toProperCase, random, wait, kernelVersion, databaseVersion) {
     this.toProperCase = (string)  => {
       return string.replace(/([^\W_]+[^\s-]*) */g, (txt) => {
-       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
       });
-    }  
+    };  
+
+
+    this.kernelVersion = thisPackage.version;
+    this.databaseVersion = thisPackage.dependencies.enmap.slice(1);
     
     
     //Makes the string in the function proper case. For example, "hi how are you today" would become "Hi How Are You Today".
 
     this.toInvertedCase = (string)  => {
       return string.replace(/([^\W_]+[^\s-]*) */g, (txt) => {
-       return txt.charAt(0).toLowerCase() + txt.substr(1).toUpperCase()
+        return txt.charAt(0).toLowerCase() + txt.substr(1).toUpperCase();
       });
-    } 
+    }; 
     
     //Does the opposite of toProperCase, makes it inverted. Like "hI hOW aRE yOU tODAY".
 
@@ -30,4 +37,4 @@ class utils {
   }
 }
 
-module.exports = utils
+module.exports = utils;
