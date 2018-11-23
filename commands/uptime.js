e26ef4a8e43e2356d2d32d4c0a222beb7680c0b4
Matthew Ray <matthew_ray@corporate.tssservicecollc.com>
2018-11-23 17:39:14 -0500

    Update commands/uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,19 +4,21 @@ const moment = require('moment');
 require('moment-duration-format');
 
 
-exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
+exports.run = (client, message) => {
   if (talkedRecently.has(message.author.id) && !message.member.roles.has('490364533550874644')) {
 
     message.channel.send('You are being rate limited!' + message.author);
   } else { 
     const duration = moment.duration(client.uptime).format(' D [days], H [hrs], m [mins], s [secs]');
     const embed = new Discord.RichEmbed()
-      .setAuthor(`${client.user.username}`, `${client.user.avatarURL}`)
+      //.setAuthor(`${client.user.username}`, `${client.user.avatarURL}`)
+      .setThumbnail(client.user.avatarURL)
       .setColor(message.member.displayColor)
       .addField('• Uptime', `${duration}`, false)
       .addField('• API Latency', `${Math.round(client.ping)}ms`, true)
       .addField('• API Ping Ratings', `${client.pings.join(', ')}`, true)
-      .setFooter(`${client.user.username} | Beta - Master`);
+      .setFooter(client.user.username, client.user.avatarURL)
+      .setTimestamp();
     
 
     message.channel.send(embed);
