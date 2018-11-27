1c42d5959547adafa1db2a0a5d06b09c9d350e22
Prylaris <prylaris.git@gmail.com>
2018-11-27 15:07:09 -0500

    moar moar changes

--- monitor.bat
+++ monitor.bat
@@ -1,7 +1,7 @@
 @echo off
-title Moonglow PM2 Dashboard
+title Garnet PM2 Dashboard
 <<<<<<< HEAD
-pm2 monit Moonglow
+pm2 monit Garnet
 =======
 pm2 monit 0
 >>>>>>> 6ab0b8b91f3f470fd15e8bced0c6c324cc169dc1
