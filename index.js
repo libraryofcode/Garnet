325f0ec6a37f38decebed0d6c6a51a52bf21ccfb
Matthew R <40540813+mattsilver1@users.noreply.github.com>
2018-12-03 00:19:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ setTimeout(() => {
   });
 
   
-  const server = app.listen(81, () => {
+  const server = app.listen(8800, () => {
     console.log(`Express running → PORT ${server.address().port}`);
     app.set('title', 'Moonglow Help Manual');
     //app.use(favicon(__dirname + '/util/server/garnet.ico'));
